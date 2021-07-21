50080f594d23b8fad385f57a70f84d791298c5c1
AndreaChavez11 <andrea.chavez180@comunidadunir.net>
2021-07-21 07:14:11 +0200

    Avance nfs y configuraci{on de nodos master y workers

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -3,4 +3,5 @@
 # añadir tantas líneas como sean necesarias para el correcto despligue
 ansible-playbook -i host.lab install-common.yaml
 ansible-playbook -i host.lab -l nfs install-nfs.yaml
+ansible-playbook -i host.lab install-servers.yaml
 #ansible-playbook -i host.lab install-kubernetes.yaml
