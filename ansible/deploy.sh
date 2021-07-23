0884da726dfe36665bcbfdfeb7ab8c7183a175ea
AndreaChavez11 <andrea.chavez180@comunidadunir.net>
2021-07-23 05:37:00 +0200

    Configuracion y join de workers

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -5,5 +5,5 @@ ansible-playbook -i host.lab install-common.yaml
 ansible-playbook -i host.lab -l nfs install-nfs.yaml
 ansible-playbook -i host.lab conf-servers.yaml
 ansible-playbook -i host.lab -l master conf-kubernetes.yaml
-
+ansible-playbook -i host.lab -l workers conf-workers.yaml
 
